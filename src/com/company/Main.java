64ebec45037740bfbda0c69ae5af1c234f0ab9c1
Timofey-K <timca1997@mail.ru>
2019-11-07 11:54:18 +0300

    FT-12 V1.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,11 +9,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите число");
         int test = sc.nextInt();
-
+// проверка, число делится на 15 без остатка?
         Proverka p1 = (test1 -> {if (test1%15 == 0) {return true;} else {return false;}});
-
+// проверка, число положительное?
         Proverka p2 = (test1 -> {if (test1 >= 0) {return true;} else {return false;}});
-
+// проверка, число не принадлежит к числам фибоначчи
         Proverka p3 = (test1 -> {
             int n0 = 0;
             int n1 = 1;
@@ -28,7 +28,7 @@ public class Main {
                 n0 = n1;
                 n1 = n2;
             }
-            if (r) {return true;} else {return false;}});
+            if (r) {return false;} else {return true;}});
 
         System.out.println(p1.prov(test));
         System.out.println(p2.prov(test));
